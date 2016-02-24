f689bbe0d3860651253163d146b660580829ed17
WangLe <Lele@DN0a2257c3.SUNet>
2016-02-23 20:00:37 -0800

    servo test

---
+++ servo_test/servo_test.ino
@@ -0,0 +1,31 @@
+const int pinNum = 10;
+int onTime;
+int offTime;
+  
+void setup() {
+  pinMode(pinNum, OUTPUT);
+  Serial.begin(9600);
+  onTime = 1000;
+  offTime = 20000-onTime;
+}
+void loop() {
+  if (Serial.available()) {
+    int inChar = Serial.read();
+    if (inChar == '1') {
+      onTime = 1000;
+    } else if (inChar == '2') {
+      onTime = 1500;
+    } else if (inChar == '3') {
+      onTime = 2000;
+    }
+    offTime = 20000 - onTime;
+    Serial.println(onTime);
+    for (int i=0; i<100; ++i) {
+      digitalWrite(pinNum,HIGH);
+      delayMicroseconds(onTime);
+      digitalWrite(pinNum,LOW);
+      delayMicroseconds(offTime);
+    }
+  }
+}
+
